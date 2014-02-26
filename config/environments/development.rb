4a87f2975185775052bd763a869ece871372f741
Ravi <ravivendan@gmail.com>
2014-02-26 09:40:16 -0500

    Signin & regestration pages CSS done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,9 @@
 CPC::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+# Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+# In the development environment your application's code is reloaded on
+# every request. This slows down response time but is perfect for development
+# since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -26,4 +26,18 @@ CPC::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "rails.ravi@gmail.com",
+    :password => "vendan_rails",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
