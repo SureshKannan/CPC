ff4a7f3baac8a76e76704728cdd6ed6916d9302d
Ravi <ravivendan@gmail.com>
2014-03-28 22:12:41 -0400

    clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,7 @@ class ApplicationController < ActionController::Base
     
   end
   
-    def after_sign_in_path_for(resource)
-
-    '/my_devise/sessions/thankyou'
-
-  end
   
-   def after_sign_up_path_for(resource)
-
-    '/my_devise/sessions/thankyou'
-
-  end
   
   # def after_sign_in_path_for(resource)
     # sign_in_url = url_for(:action => 'new', :controller => 'sessions' , :only_path => false, :protocol => 'http')
